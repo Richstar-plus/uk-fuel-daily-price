28efa98b83db9e3b8fa317a07bc6bd4895da9503
richard <richardsunday0812@gmail.com>
2026-03-25 13:56:35 +0100

    Refactor HomePage to integrate TrendGraph component and streamline fuel filtering functionality

---
+++ src/components/TrendGraph.jsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import BrandChart from "../components/BrandChart";
+import { transformBrandData } from "../utils/transformBrandData";
+
+export function TrendGraph({ stations }) {
+  const [fuelFilter, setFuelFilter] = useState("");
+  const [fuelFilterValue, setFuelFilterValue] = useState("");
+
+    const chartData = transformBrandData(stations, "E10");
+  return (
+    <>
+      <div className="trend-graph-container">
+        <div className="trend-graph-search">
+          <select
+            name="search"
+            className="search-select"
+            value={fuelFilter}
+            onChange={(e) => setFuelFilter(e.target.value)}
+          >
+            <option value="search by fuel type">Search by Fuel Type</option>
+            <option value="search by location">Search by Location</option>
+          </select>
+
+          {fuelFilter === "search by fuel type" && (
+            <div className="fuel-type-input-container">
+              <select
+                name="fuel-type"
+                className="fuel-type"
+                value={fuelFilterValue}
+                onChange={(e) => setFuelFilterValue(e.target.value)}
+              >
+                <option value="">Select...</option>
+                <option value="E10">E10</option>
+                <option value="SDV">SDV</option>
+                <option value="E5">E5</option>
+              </select>
+              <button className="search-btn">Search</button>
+            </div>
+          )}
+        </div>
+        <div className="trend-graph-contaner">
+          <div className="trend-graph">
+            <div className="trend-graph-title">Brand Prices</div>
+            <BrandChart data={chartData} />
+          </div>
+          <div className="trend-list">
+            <h5>Brands and Address</h5>
+            <div className="station-list">
+              <table>
+                <thead>
+                  <tr>
+                    <th>Brand</th>
+                    <th>Address</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {stations.map((station) => (
+                    <tr key={station.id}>
+                      <td>{station.brand}</td>
+                      <td>{station.address}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,19 @@
 import { useLoaderData } from "react-router-dom";
 import { Card } from "../components/Cards";
 import "./Home.css";
-import BrandChart from "../components/BrandChart";
-import { transformBrandData } from "../utils/transformBrandData";
+import { TrendGraph } from "../components/TrendGraph";
+
 import { useState } from "react";
 
 export function HomePage() {
   const [filter, setFilter] = useState("");
   const [filterValue, setFilterValue] = useState("");
 
-  const [fuelFilter, setFuelFilter] = useState("");
-  const [fuelFilterValue, setFuelFilterValue] = useState("");
+
 
   const fuel = useLoaderData();
   const stations = fuel.data || [];
   console.log(fuel);
-  const chartData = transformBrandData(stations, "E10");
   return (
     <>
       <div className="main-content">
@@ -38,62 +36,8 @@ export function HomePage() {
             station="Jet Station, Leads"
           />
         </div>
-        <div className="trend-graph-container">
-          <div className="trend-graph-search">
-            <select
-              name="search"
-              className="search-select"
-              value={fuelFilter}
-              onChange={(e) => setFuelFilter(e.target.value)}
-            >
-              <option value="search by fuel type">Search by Fuel Type</option>
-              <option value="search by location">Search by Location</option>
-            </select>
+        <TrendGraph stations={stations} />
 
-            {(fuelFilter === "search by fuel type") && (
-              <div className="fuel-type-input-container">
-                <select
-                 name="fuel-type" className="fuel-type"
-                  value={fuelFilterValue}
-                  onChange={(e) => setFuelFilterValue(e.target.value)}
-                >
-                  <option value="">Select...</option>
-                  <option value="E10">E10</option>
-                  <option value="SDV">SDV</option>
-                  <option value="E5">E5</option>
-                </select>
-                <button className="search-btn">Search</button>
-              </div>
-            )}
-          </div>
-          <div className="trend-graph-contaner">
-            <div className="trend-graph">
-              <div className="trend-graph-title">Brand Prices</div>
-              <BrandChart data={chartData} />
-            </div>
-            <div className="trend-list">
-              <h5>Brands and Address</h5>
-              <div className="station-list">
-                <table>
-                  <thead>
-                    <tr>
-                      <th>Brand</th>
-                      <th>Address</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {stations.map((station) => (
-                      <tr key={station.id}>
-                        <td>{station.brand}</td>
-                        <td>{station.address}</td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </div>
-        </div>
 
         <div className="list-main-holder">
           <div className="filter-select">
